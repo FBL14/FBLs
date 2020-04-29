fe9f465d733e060a0c644512ddb364958055b35d
FBL14 <64152205+FBL14@users.noreply.github.com>
2020-04-29 13:30:01 +0200

    Update game_easy.js

--- game_easy.js
+++ game_easy.js
@@ -1,3 +1,4 @@
+//przypisanie kartą kolorów.
 const cardColors = ["red", "red", "green", "green", "blue", "blue", "brown", "brown", "yellow", "yellow", "gray", "gray", "cadetblue", "cadetblue", "violet", "violet", "lightgreen", "lightgreen"];
 
 let cards = document.querySelectorAll('div');
